ac09386cf5cf2ae883e9ec8ae708d2c746b4c78e
bfimberg <bfimberg@gmail.com>
2023-03-13 17:02:27 +0200

    Complete writing to database, fix some pylint problems

--- get_data.py
+++ get_data.py
@@ -1,15 +1,13 @@
-"""Module providing necessary tools for tabular data manipulation"""
-import pandas as pd
-"""Module for storing the data"""
 import sqlite3
+import pandas as pd
 
 # Load the dataset from url
-url = 'https://gist.githubusercontent.com/curran/' \
+URL = 'https://gist.githubusercontent.com/curran/' \
       'a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/' \
       'iris.csv'
 
 # Create dataframe
-iris_dataframe = pd.read_csv(url)
+iris_dataframe = pd.read_csv(URL)
 
 # Remove edge-cases (2 standard deviations away from mean)
 iris_numeric = iris_dataframe.select_dtypes(exclude=['object'])
@@ -20,7 +18,7 @@ iris_numeric = iris_numeric[
       ].dropna()
 iris_dataframe.update(iris_numeric)
 
-# Create new columns containing: ratio of sepal length and sepal width,  ratio of sepal length and petal length
+# Create new columns: ratio of sepal length and sepal width,  ratio of sepal length and petal length
 iris_dataframe['sepal_ratio'] = iris_dataframe['sepal_length'] / iris_dataframe['sepal_width']
 iris_dataframe['length_ratio'] = iris_dataframe['sepal_length'] / iris_dataframe['petal_length']
 iris_dataframe['sepal_ratio'] = iris_dataframe['sepal_ratio'].astype(float).round(2)
@@ -33,4 +31,6 @@ iris_dataframe.to_csv('iris_with_transformations', index=False)
 conn = sqlite3.connect('iris.db')
 
 # Write the gathered and transformed data to the database
-iris_dataframe.to_sql('iris_with_transformations', conn)
+iris_dataframe.to_sql('iris_with_transformations', conn, if_exists='replace', index=False)
+
+conn.close()
